605d6391a2b506b8dd95d5e8be1e1192c510ca4c
Vivienne Benke <vivienne.benke@gmail.com>
2018-10-29 19:04:54 +0100

    Added crude login page.

---
+++ Wedding/ClientApp/src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+})
+/** login component*/
+export class LoginComponent {
+    /** login ctor */
+    constructor() {
+
+    }
+}
